8712b0d912fc26b0771e74d5c2fe73de6cfd9008
romejoseph <romejosephsantos@gmail.com>
2026-06-17 02:19:12 +0800

    use Lombok

--- src/main/java/com/rjrs/xpense/dto/CreateExpenseRequest.java
+++ src/main/java/com/rjrs/xpense/dto/CreateExpenseRequest.java
@@ -1,7 +1,6 @@
 package com.rjrs.xpense.dto;
 
 import com.rjrs.xpense.entity.ExpenseCategory;
-import org.springframework.cglib.core.Local;
 
 import java.math.BigDecimal;
 import java.time.LocalDate;
--- src/main/java/com/rjrs/xpense/entity/Expense.java
+++ src/main/java/com/rjrs/xpense/entity/Expense.java
@@ -4,6 +4,7 @@ import jakarta.persistence.*;
 import jakarta.validation.constraints.NotBlank;
 import jakarta.validation.constraints.NotNull;
 import jakarta.validation.constraints.Positive;
+import lombok.Data;
 import org.hibernate.annotations.CreationTimestamp;
 
 import java.math.BigDecimal;
@@ -12,6 +13,7 @@ import java.time.LocalDateTime;
 import java.util.UUID;
 
 @Entity
+@Data
 public class Expense {
     @Id
     @GeneratedValue(strategy = GenerationType.UUID)
@@ -38,5 +40,9 @@ public class Expense {
         this.amount = amount;
         this.category = category;
         this.expenseDate = expenseDate;
+    }
+
+    public Expense() {
+
     }
 }
